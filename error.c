99f82bc98c20fae542e6deda653782d30620d1cb
benardidoko <benardidoko@yahoo.com>
2023-11-15 08:15:55 -0800

    tired

---
+++ error.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_error - Calls the appropriate error function based on the type
+ *              of error (builtin, syntax, or permission).
+ * @datash: Data structure that contains arguments.
+ * @eval: Error value.
+ * Return: Error code.
+ */
+int get_error(data_shell *datash, int eval)
+{
+	char *error;
+
+	switch (eval)
+	{
+	case -1:
+		error = error_env(datash);
+		break;
+	case 126:
+		error = error_path_126(datash);
+		break;
+	case 127:
+		error = error_not_found(datash);
+		break;
+	case 2:
+		if (_strcmp("exit", datash->args[0]) == 0)
+			error = error_exit_shell(datash);
+		else if (_strcmp("cd", datash->args[0]) == 0)
+			error = error_get_cd(datash);
+		break;
+	}
+
+	if (error)
+	{
+		write(STDERR_FILENO, error, _strlen(error));
+		free(error);
+	}
+
+	datash->status = eval;
+	return (eval);
+}
+
